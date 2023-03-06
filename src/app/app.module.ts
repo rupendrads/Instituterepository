ebb2a878ef583cecee2c0005680b79d7a7d617c7
Pravin <pravin.kolambkar@gmail.com>
2023-03-06 21:01:04 +0530

    Course commited

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+
 import { NewAdmission } from './admission/new-admission/new-admission.component';
 import { AppComponent } from './app.component';
 import { NewUserComponent } from './user/new-user/new-user.component';
 import { SignInComponent } from './authentication/sign-in/sign-in.component';
+import { NewCourseComponent } from './course/new-course/new-course.component';
+import { NewCourseEditComponent } from './course/new-course/new-course-edit/new-course-edit.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [    
+    AppComponent,
     NewUserComponent,
     SignInComponent,    
     NewAdmission,
-    AppComponent
+    CourseComponent,
+    NewCourseComponent,
+    NewCourseEditComponent
   ],
   imports: [
     BrowserModule,
