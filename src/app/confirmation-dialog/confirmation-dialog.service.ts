a0f8e74cc64e285baa38669148742f2f49c939e2
Pravin <pravin.kolambkar@gmail.com>
2023-03-31 18:10:47 +0530

    confirm dialog update

--- src/app/confirmation-dialog/confirmation-dialog.service.ts
+++ src/app/confirmation-dialog/confirmation-dialog.service.ts
@@ -11,13 +11,11 @@ export class ConfirmationDialogService {
   constructor(private modalService: NgbModal) { }
 
   public confirm(
-    title: string,
     message: string,
     btnOkText: string = 'OK',
     btnCancelText: string = 'Cancel',
     dialogSize: 'sm'|'lg' = 'sm'): Promise<boolean> {
     const modalRef = this.modalService.open(ConfirmationDialogComponent, { size: dialogSize });
-    modalRef.componentInstance.title = title;
     modalRef.componentInstance.message = message;
     modalRef.componentInstance.btnOkText = btnOkText;
     modalRef.componentInstance.btnCancelText = btnCancelText;
