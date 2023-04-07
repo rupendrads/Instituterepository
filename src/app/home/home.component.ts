d801708b982cdf9c15084259d79075e382c5d203
Pravin <pravin.kolambkar@gmail.com>
2023-04-07 15:09:37 +0530

    header comp changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,36 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { AuthService } from '../authentication/services/auth.service';
 
 @Component({
     selector:'home',
     templateUrl: '/home.component.html',
     styleUrls: ['/home.component.css']
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+    isUserLoggedIn:boolean = false;
+    loggedInUserName:string|undefined = undefined;
+    loggedInUserId:string|undefined = undefined;
+    loggedInUserType: string|undefined = undefined;
+
+    constructor(private authService:AuthService){        
+    }
+
+    ngOnInit(){
+        this.authService.userLoggedInSubject.subscribe({
+          next: (result: any) => {
+            console.log(result);
+            this.isUserLoggedIn = result.isUserLoggedIn;
+            this.loggedInUserId = result.isUserLoggedIn == true? result.loggedInUserId : undefined;
+            this.loggedInUserType = result.isUserLoggedIn  == true? result.loggedInUserType : undefined;
+            console.log(this.loggedInUserType);
+          },
+          error: (e)=> console.log(e),
+          complete: () => console.log("complete")      
+        });
+      }
+    
+      signOut(){
+        this.authService.logout();
+      }
+
+}
