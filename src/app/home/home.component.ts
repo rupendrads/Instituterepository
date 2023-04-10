895e5913077142958eb7a4297da1170aaf7095ef
Pravin <pravin.kolambkar@gmail.com>
2023-04-10 19:01:26 +0530

    Home Component 1st commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,15 +3,13 @@ import { AuthService } from '../authentication/services/auth.service';
 
 @Component({
     selector:'home',
-    templateUrl: '/home.component.html',
-    styleUrls: ['/home.component.css']
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
     isUserLoggedIn:boolean = false;
     loggedInUserName:string|undefined = undefined;
-    loggedInUserId:string|undefined = undefined;
-    loggedInUserType: string|undefined = undefined;
-
+    
     constructor(private authService:AuthService){        
     }
 
@@ -20,9 +18,8 @@ export class HomeComponent implements OnInit {
           next: (result: any) => {
             console.log(result);
             this.isUserLoggedIn = result.isUserLoggedIn;
-            this.loggedInUserId = result.isUserLoggedIn == true? result.loggedInUserId : undefined;
-            this.loggedInUserType = result.isUserLoggedIn  == true? result.loggedInUserType : undefined;
-            console.log(this.loggedInUserType);
+            
+            console.log(this.isUserLoggedIn);
           },
           error: (e)=> console.log(e),
           complete: () => console.log("complete")      
